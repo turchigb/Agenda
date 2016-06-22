aabf13b35fccca561613f4c584da1a967157cab3
gbturchi <gbturchi@gmail.com>
2016-06-22 13:15:44 -0700

    Agenda - fino al 22 giugno ore 22:16

---
+++ Agenda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Agenda
+//
+//  Created by Mattei on 22/06/16.
+//  Copyright © 2016 Mattei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
